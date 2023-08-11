ab57e7c767b675e91d4741d09dd3085846e23390
Martin Myrseth <mm@myme.no>
2023-08-11 11:45:12 +0200

    Simplify flake
     * Use "pinned" (local) nixpkgs
     * No need for flake-utils

--- flake.nix
+++ flake.nix
@@ -1,30 +1,25 @@
 {
   description = "A dev sandbox";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        ghc = pkgs.ghc.withPackages (hs: with hs; [
-          bytestring
-          cryptonite
-          random
-        ]);
-        python = pkgs.python3.withPackages (ps: with ps; [
-          ipython
-        ]);
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            ghc
-            pkgs.nodejs
-            python
-          ];
-        };
-      });
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      ghc = pkgs.ghc.withPackages (hs: with hs; [
+        bytestring
+        cryptonite
+        random
+      ]);
+      python = pkgs.python3.withPackages (ps: with ps; [
+        ipython
+      ]);
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          ghc
+          pkgs.nodejs
+          python
+        ];
+      };
+    };
 }
